7e917064f77f303cdb8908f26bc4d2385a00c506
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-03-12 13:23:00 +0100

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subset: ["latin"],
+});
+
+export const metadata = {
+  title: "Billio",
+  description: "The best expense tracking app for your house/apartment.",
+  keywords: "expense tracker, utility expense tracker, apartment, house, landlord"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
