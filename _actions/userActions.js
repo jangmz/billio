b1bad84b9607226cb25f71dc50d4d439f8879af0
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-03-17 14:44:51 +0100

    queries for read, update and delete

--- _actions/userActions.js
+++ _actions/userActions.js
@@ -9,8 +9,6 @@ export async function insertUser(userData) {
         await connectDB();        
         const newUser = new User(userData);
         const savedUser = newUser.save();
-
-        console.log("User saved");
         return savedUser;
     } catch (error) {
         return { error: error.message };
@@ -22,9 +20,44 @@ export async function getUser(email) {
     try {
         await connectDB();
         const user = await User.findOne({ email });
-
-        console.log("User found already exists");
         return user;
+    } catch (error) {
+        return { error: error.message };
+    }
+}
+
+// retrieve user by id
+export async function getUserById(id) {
+    try {
+        await connectDB();
+        const user = await User.findById(id);
+        return user;
+    } catch (error) {
+        return { error: error.message };
+    }
+}
+
+// update user by id
+export async function updateUserById(id, userData) {
+    try {
+        await connectDB();
+        const updatedUser = await User.findByIdAndUpdate(
+            id, // _id of the document to update
+            userData, // update operation -> data that have values will be updated
+            { new: true } // option to return the updated document
+        );        
+        return updatedUser;
+    } catch (error) {
+        return { error: error.message };
+    }
+}
+
+// delete user by id
+export async function deleteUserById(userId) {
+    try {
+        await connectDB();
+        const deletedUser = await User.findByIdAndDelete(userId);
+        return deletedUser;
     } catch (error) {
         return { error: error.message };
     }
