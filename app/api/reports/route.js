9ac0d6b66f0a84b3ce326bb4dd7f578dbac2c033
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-03-21 14:18:58 +0100

    creating a report depending on the given timeframe sorted by user residences and categories

---
+++ app/api/reports/route.js
@@ -0,0 +1,100 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/config/auth";
+import { getReport } from "@/_actions/residenceActions";
+
+// GET /api/reports?timeframe=[mothly/quarterly/yearly] -> return all bills sorted by residence -> sorted bills by category
+export async function GET(req) {
+    try {
+        // check session
+        /*const session = await auth();
+        
+        if (!session) {
+            const error = new Error("Not authorized");
+            error.status = 401;
+            throw error;
+        }*/
+
+        // retrieve timeframe
+        const userId = "67d4290bbe6ed5a063405432";
+        const { searchParams } = new URL(req.url)
+        const timeFrame = searchParams.get("timeframe"); // monthly, quarterly, yearly
+
+        // define timeframe filter based on createdAt -> later change this to dueDate
+        let matchStage = {};
+        const now = new Date();
+
+        if (timeFrame === "monthly") {
+            matchStage = {
+                createdAt: {
+                    $gte: new Date(now.getFullYear(), now.getMonth(), 1),
+                    $lte: new Date(now.getFullYear(), now.getMonth() + 1, 1)
+                }
+            }
+        } else if (timeFrame === "quarterly") {
+            const quarterStartMonth = Math.floor(now.getMonth() / 3) * 3;
+            matchStage = {
+                createdAt: {
+                    $gte: new Date(now.getFullYear(), quarterStartMonth, 1),
+                    $lt: new Date(now.getFullYear(), quarterStartMonth + 3, 1),
+                },
+            };
+        } else if (timeFrame === "yearly") {
+            matchStage = {
+                createdAt: {
+                    $gte: new Date(now.getFullYear(), 0, 1),
+                    $lt: new Date(now.getFullYear() + 1, 0, 1),
+                },
+            };
+        }
+
+        // query
+        console.log(userId, matchStage.createdAt.$gte); //ok
+        const report = await getReport(userId, matchStage);
+
+        if (report?.error || !report) throw new Error(report?.error || "Failed to create report.");
+
+        return NextResponse.json(
+            { message: "Report generated", data: report },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { error: error.message || "Internal server error" },
+            { status: error.status || 500 }
+        );
+    }
+}
+
+/* 
+=====  REPORT STRUCTURE =====
+Residences [
+    Residence {
+    name, address, ... // example: Lake Cottage
+    Categories [
+        Category {
+            name, ... // example: Water
+            totalAmount: 562.2,
+            Bills [
+                bill { month: January, amount: 62.5, ...},
+                bill { month: February, amount: 78.6, ...},
+                bill { month: March, amount: 66.5, ...},
+                ... // etc. all other bills
+            ]
+        },
+        Category {
+            name, ... // example: Electricity
+            totalAmount: 837.3,
+            Bills [
+                bill { month: January, amount: 62.5, ...},
+                bill { month: February, amount: 78.6, ...},
+                bill { month: March, amount: 66.5, ...},
+                ... // etc. all other bills
+            ]
+        }, 
+        ... // etc. all other categories
+    ]
+},
+... // etc. all other residences
+]
+
+*/
