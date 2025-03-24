2ba9e8bd48389ecbf86690f4c243878feeb087e0
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-03-24 14:26:09 +0100

    added navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const poppins = Poppins({
   weight: ["400", "700"],
@@ -15,7 +16,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body>
+        <Navbar />
         {children}
       </body>
     </html>
