f48fb15ef81d4dd4e0fe6c30b151bcbaa4c7cbc1
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-03-25 14:42:05 +0100

    pro-con icon and text component

---
+++ components/ProConIcontext.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useState } from 'react';
+import { FaCheck, FaXmark } from "react-icons/fa6";
+
+export default function ProConIcontext({ icon, text }) {
+    const [textColor, setTextColor] = useState(() => {
+        if (icon === "pro") return "success";
+        if (icon === "con") return "error";
+        if (icon === "neutral") return "secondary";
+    });
+  return (
+    <div className={`flex items-center gap-1 text-${textColor}`}>
+        {
+            (icon === "pro" || icon === "neutral") && <FaCheck /> || icon === "con" && <FaXmark />
+        }
+        {text}
+    </div>
+  )
+}
