ccc298283d1dd4fc2c99bd74099b379d16089618
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-03-26 13:28:14 +0100

    Changed text

--- components/sections/HeroSection.jsx
+++ components/sections/HeroSection.jsx
@@ -1,45 +1,57 @@
-import React from 'react';
-import Button from '../buttons/Button';
-import Link from 'next/link';
-import ProConIcontext from '../ProConIcontext';
+import React from "react";
+import Button from "../buttons/Button";
+import Link from "next/link";
+import HeroImage from "@/public/hero1.jpg";
+import ProConIcontext from "../ProConIcontext";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
     <section id="hero" className="relative overflow-hidden">
-        <div className="mx-auto flex justify-center max-w-7xl px-5 py-10">
-            <div className="flex flex-col items-center gap-8">
-                <h1 className="font-bold text-5xl text-center">
-                    Effortlessly Manage Your Housing Expenses With Billio
-                </h1>
-                <div>
-                    <ProConIcontext
-                        icon={"pro"}
-                        text={"Track all housing bills in one place"}
-                    />
-                    <ProConIcontext
-                        icon={"pro"}
-                        text={"Track multiple properties (homes, apartments, rentals)"}
-                    />
-                    <ProConIcontext
-                        icon={"pro"}
-                        text={"Simplified expense management"}
-                    />
-                    <ProConIcontext
-                        icon={"pro"}
-                        text={"Stay organized and financially savvy"}
-                    />
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-2xl">
+                    <h1 className="text-5xl font-bold">
+                        Effortlessly Manage Your Housing Expenses With Billio
+                    </h1>
+                    <div className="py-6">
+                        <div>
+                            Everything you need to track and manage all your houses, apartments, rentals in one place. Simplified expense management to stay organized and financially savvy.
+                        </div>
+                    </div>
+                    {/*<div className="py-6">
+                        <ProConIcontext
+                            icon={"pro"}
+                            text={"Track all housing bills in one place"}
+                        />
+                        <ProConIcontext
+                            icon={"pro"}
+                            text={"Track multiple properties (homes, apartments, rentals)"}
+                        />
+                        <ProConIcontext
+                            icon={"pro"}
+                            text={"Simplified expense management"}
+                        />
+                        <ProConIcontext
+                            icon={"pro"}
+                            text={"Stay organized and financially savvy"}
+                        />
+                    </div>*/}
+                    <Link href="/login"> {/* link to stripe checkout */}
+                        <Button 
+                            text={"GET ACCESS"}
+                            btnStyle={"btn-primary"}
+                        />
+                    </Link>
                 </div>
-                <Link href="/login"> {/* link to stripe checkout */}
-                    <Button 
-                        text={"GET ACCESS"}
-                        btnStyle={"btn-primary"}
-                    />
-                </Link>
-                
             </div>
-            <div>
-                {/* app image */}
-            </div>
+            {/*<div>
+                <Image 
+                    src={HeroImage} 
+                    alt="Hero image" 
+                    className="max-w-2xl rounded-lg shadow-2x1"
+                />
+            </div>*/}
         </div>
     </section>
   )
