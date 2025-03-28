f4bceaa785f9043a9539f0a972eb748577c750b7
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-03-28 08:12:27 +0100

    new section added

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import HeroSection from "@/components/sections/HeroSection";
 import PropertiesSection from "@/components/sections/PropertiesSection";
 import ProblemsSection from "@/components/sections/ProblemsSection";
 import PricingSection from "@/components/sections/PricingSection";
+import FaqSection from "@/components/sections/FaqSection";
 
 export default async function Home() {
   const session = await auth();
@@ -26,6 +27,7 @@ export default async function Home() {
       <PropertiesSection />
       <ProblemsSection />
       <PricingSection />
+      <FaqSection />
     </div>
   );
 }
