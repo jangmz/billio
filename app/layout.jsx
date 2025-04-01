cb6fc087048821a13ff79a165ff6edbc5e053c14
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-04-01 14:39:12 +0200

    navigation bar is displayed if user is not logged in

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,8 +23,10 @@ export default async function RootLayout({ children }) {
     <html lang="en" className="h-full">
       <body className="flex flex-col min-h-screen">
         <header className="sticky top-0 z-50">
-          {
+          {/*
             session?.user ? <UserNavbar /> : <Navbar />
+          */
+            !session?.user && <Navbar />
           }
         </header>
         <main className="flex-1">
