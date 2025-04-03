42437b22c6cb03104eff419de49d8bee42b05950
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-04-03 09:44:38 +0200

    auth.js configuration

---
+++ auth.js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { getUser } from "./_actions/userActions";
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+    providers: [
+        GitHub
+    ],
+    pages: {
+        signIn: "/login",
+        error: "/login",
+        signOut: "/logout",
+    },
+    session: {
+        strategy: "jwt"
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            // adding user ID to the token when user logs in
+            if (user) {
+                // retrieve user ID from database
+                const existingUser = await getUser(user.email);
+
+                if (existingUser) {
+                    token.id = existingUser.id; // save user ID in token
+                } else {
+                    console.error("User not found in the database:", user.email);
+                }
+            }            
+            return token;
+        },
+        async session({ session, token }) {
+            // add user ID from token to the session
+            if (session?.user) {
+                session.user.id = token.id;
+            }
+
+            return session;
+        },
+    },
+});
