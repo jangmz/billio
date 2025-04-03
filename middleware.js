30eec88699ee2f72724a7584e21206d27da64141
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-04-03 10:22:38 +0200

    removed comments

--- middleware.js
+++ middleware.js
@@ -14,6 +14,8 @@ const protectedRoutes = [
 export default async function middleware(req) {
     const session = await auth();
     const { pathname } = req.nextUrl; // current pathname
+
+    // check if current route is protected
     const isProtected = protectedRoutes.some((route) => 
         pathname.startsWith(route)
     );
@@ -24,31 +26,3 @@ export default async function middleware(req) {
 
     return NextResponse.next();
 }
-
-/*import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
-
-export async function middleware(req) {
-    console.log("============Middleware============")
-
-    const cookie = req.cookies.get("authjs.session-token")?.value;
-    console.log("Cookie:", cookie);
-
-    const token = await getToken({ req, secret: process.env.AUTH_SECRET, cookie });
-    console.log("Token:", token);
-
-    if (!token) {
-        // return NextResponse.redirect(new Url("/login", req.url));
-        return NextResponse.json(
-            { message: "Not authorized" },
-            { status: 403 }
-        );
-    }
-
-    return NextResponse.next();
-}
-*/
-/*export const config = {
-    // these routes are protected
-    matcher: ["/dashboard/:path*", "/api/:path*"] 
-};*/
