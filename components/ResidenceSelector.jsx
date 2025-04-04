91e189749f2663037c8659939388694e01987ed4
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-04-04 14:47:06 +0200

    dropdown for selecting residences

---
+++ components/ResidenceSelector.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
+export default function ResidenceSelector({ residences }) {
+    const [selectedResidence, setSelectedResidence] = useState(residences[0]);
+
+    function changeResidence(e) {
+        e.preventDefault();
+        setSelectedResidence(residences.find(res => res._id === e.target.value));
+    }
+
+    return (
+        <div>
+            <select onChange={changeResidence}>
+                {
+                    residences.map((residence) => (
+                        <option key={residence._id} value={residence._id}>{residence.name} ({residence.address})</option>
+                    ))
+                }
+            </select>
+        </div>
+    )
+}
