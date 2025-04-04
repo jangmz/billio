0535d2eee43248cd8cf81b17bb60fecc496fb52c
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-04-04 14:10:04 +0200

    style updated

--- components/cards/DashResidenceCard.jsx
+++ components/cards/DashResidenceCard.jsx
@@ -1,10 +1,10 @@
 export default function DashResidenceCard({ residence }) {
   return (
     <div className="flex flex-col items-start justify-start h-32 rounded-sm p-3 bg-gray-200">
-        <p className="text-lg text-gray-400 dark:text-gray-500">
+        <p className="text-lg">
             {residence.name}
         </p>
-        <p>
+        <p className="text-sm text-gray-500">
             {residence.address}
         </p>
     </div>
