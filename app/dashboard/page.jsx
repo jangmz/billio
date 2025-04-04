f72574937942cf676c33ca1b7a7483f25ee8a8e8
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-04-04 14:09:51 +0200

    base dash layout

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
-import { auth } from "@/auth";
 import { validateSession } from "@/config/validateSession";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import DashResidenceCard from "@/components/cards/DashResidenceCard";
+import DashTitle from "@/components/DashTitle";
 
 const apiUrl = process.env.API_URL;
 
@@ -9,7 +9,6 @@ export default async function Dashboard() {
     try {
         // validate session
         const session = await validateSession();
-        //console.log("Session in dashboard:", session); // session data OK
 
         // retrieve cookies
         const cookieStore = cookies();
@@ -20,19 +19,10 @@ export default async function Dashboard() {
             method: "GET",
             headers: { 
                 "Content-Type": "application/json",
-                Cookie: `authjs.session-token=${sessionToken}` // session cookie is sent explicitly
+                Cookie: `authjs.session-token=${sessionToken}`
             },
             cache: "no-store"
         });
-
-        /*const response = await fetch(`${apiUrl}/test`, {
-            method: "GET",
-            headers: { 
-                "Content-Type": "application/json",
-                Cookie: `authjs.session-token=${sessionToken}` // session cookie is sent explicitly
-            },
-            cache: "no-store"
-        });*/
 
         if (!response.ok) {
             const errData = await response.json();
@@ -40,16 +30,38 @@ export default async function Dashboard() {
         }
     
         const { residences } = await response.json();
-        console.log("Fetched residences:", residences); // data OK
+        //console.log("Fetched residences:", residences); // data OK
 
         return (
             <div>
-                <h1>Welcome, {session.user.name}</h1>
-                <ul>
-                    {residences.map((residence) => (
-                        <li key={residence._id}>{residence.name}</li>
-                    ))}
-                </ul>
+                {/* 1st row */}
+                <DashTitle title="Residences" />
+                <div className="grid grid-cols-3 gap-4 mb-4">
+                    {/* TODO: components if there is no data for particular user */}
+                    {
+                        residences.map((residence) => (
+                            <DashResidenceCard key={residence._id} residence={residence} />
+                        ))
+                    }
+                </div>
+                {/* 2nd row */}
+                <DashTitle title={"Category Overview"} />
+                <div className="flex items-center justify-center h-48 mb-4 rounded-sm bg-gray-50 dark:bg-gray-800">
+                    <p className="text-2xl text-gray-400 dark:text-gray-500">
+                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
+                        </svg>
+                    </p>
+                </div>
+                {/* 3rd row */}
+                <DashTitle title={"Recent bills"} />
+                <div className="flex items-center justify-center h-48 mb-4 rounded-sm bg-gray-50 dark:bg-gray-800">
+                    <p className="text-2xl text-gray-400 dark:text-gray-500">
+                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
+                        </svg>
+                    </p>
+                </div>
             </div>
         );
     } catch (error) {
