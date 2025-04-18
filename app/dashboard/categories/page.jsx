a6a76c71706d98730969dc2c13f573a653dad782
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-04-18 14:40:00 +0200

    added dialog window for editing category data

--- app/dashboard/categories/page.jsx
+++ app/dashboard/categories/page.jsx
@@ -4,17 +4,14 @@ import { FaPlus } from "react-icons/fa6";
 import AlertError from "@/components/alerts/AlertError";
 import ButtonWithIcon from "@/components/buttons/ButtonWithIcon";
 import AlertInfo from "@/components/alerts/AlertInfo";
+import CategoryCard from "@/components/cards/CategoryCard";
 
 const apiUrl = process.env.API_URL;
 
 // endpoint -> /categories or /categories/[id] ...
 // queryParameter -> ?residence=...
 
-async function retrieveData(endpoint, queryParameter="") {
-    // retrieve cookies
-    const cookieStore = cookies();
-    const sessionToken = (await cookieStore).get("authjs.session-token")?.value;
-
+async function retrieveData(sessionToken, endpoint, queryParameter="") {
     const response = await fetch(`${apiUrl}${endpoint}${queryParameter}`, {
         method: "GET",
         headers: { 
@@ -25,13 +22,11 @@ async function retrieveData(endpoint, queryParameter="") {
 
     if (!response.ok) {
         const { error } = await response.json();
-        console.log("=======================ERROR===================")
-        console.log(error);
         throw new Error(error || "Unspecified error occured");
     }
 
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
     return data;
 }
 
@@ -40,8 +35,12 @@ export default async function CategoriesPage() {
         // validate session
         const session = await validateSession();
 
+        // retrieve cookies
+        const cookieStore = cookies();
+        const sessionToken = (await cookieStore).get("authjs.session-token")?.value;
+
         // retrieve user categories
-        const { categories } = await retrieveData("/categories");
+        const { categories } = await retrieveData(sessionToken, "/categories");
 
         return(
             <div className="flex flex-col gap-6">
@@ -49,12 +48,16 @@ export default async function CategoriesPage() {
                     <ButtonWithIcon link="#" text="New category (dialog)" icon={<FaPlus/>} />
                 </div>
                 {/* CARDS */}
-                <div className="grid-cols-1 gap-4 mb-4 md:grid-cols-2 lg:grid-cols-3">
+                <div className="grid grid-cols-1 gap-4 mb-4 md:grid-cols-2 lg:grid-cols-3">
                     {
                         categories.length !== 0 ?
                         categories.map(category => (
                             <div key={category._id} className="flex flex-col">
-                                {category.name}
+                                <CategoryCard 
+                                    category={category} 
+                                    apiUrl={apiUrl}
+                                    sessionToken={sessionToken}
+                                />
                             </div>
                         ))
                         : <AlertInfo information="No data yet" />
---
+++ components/cards/CategoryCard.jsx
@@ -0,0 +1,100 @@
+"use client";
+
+import FormFieldset from "../forms/FormFieldset";
+import AlertError from "../alerts/AlertError";
+import AlertSuccess from "../alerts/AlertSuccess";
+import Button from "../buttons/Button";
+import { useState } from "react";
+
+export default function CategoryCard({ category, apiUrl, sessionToken }) {
+    const [error, setError] = useState(null);
+    const [message, setMessage] = useState(null);
+    const [formData, setFormData] = useState({
+        name: category.name,
+    });
+
+    // changing form data
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    // updating data
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        setMessage(null);
+        setError(null);
+
+        console.log("Data for update:", formData);
+
+        try {
+            const res = await fetch(`${apiUrl}/categories/${residenceData._id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: `authjs.session-token=${sessionToken}`
+                },
+                body: JSON.stringify(formData)
+            });
+    
+            if (!res.ok) {
+                const { error } = await res.json();
+                throw new Error(error);
+            }
+
+            const { message } = await res.json();
+            setMessage(message);
+        } catch (error) {
+            console.error(error);
+            setError(error);
+        }
+    }
+
+    // for opening dialog box
+    function handleClick() {
+        document.getElementById(`update_category_${category._id}_modal`).showModal();
+    }
+
+    return (
+        <>
+            <button type="button" onClick={handleClick} className="hover:cursor-pointer">
+                <div className="flex items-center justify-center h-32 rounded-sm p-3 bg-gray-200">
+                    <p className="text-3xl">{category.name}</p>
+                </div>
+            </button>
+            <dialog id={`update_category_${category._id}_modal`} className="modal">
+                <div className="modal-box text-center">
+                    <h3 className="font-bold text-xl">Edit category</h3>
+                    <p className="py-4 text-xs">Press ESC or click outside to close</p>
+                    <div>
+                        <form onSubmit={handleSubmit} className="flex flex-col items-center gap-2">
+                            {
+                                error && <AlertError error={error} />
+                            }
+                            {
+                                message && <AlertSuccess message={message} />
+                            }
+                            <FormFieldset 
+                                title="Name"
+                                type="text"
+                                name="name"
+                                value={formData.name}
+                                onChange={(e) => handleChange(e)}
+                            />
+                            <Button
+                                text={"Update"}
+                                type={"submit"}
+                                btnStyle={"btn-primary"}
+                            />
+                        </form>
+                    </div>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>Close</button>
+                </form>
+            </dialog>
+        </>
+        
+    );
+}
