18a5c64283e7f6a81e3ebeb6cb448921a70bba87
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-04-29 11:23:25 +0200

    general component for form dropdown selector with parameters of 'text', 'name', 'options'

---
+++ components/forms/DropdownSelector.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+
+export default function DropdownSelector({ text, name, options }) {
+    const [selectedValue, setSelectedValue] = useState(options[0]);
+
+    function changeOption(e) {
+        e.preventDefault();
+        setSelectedValue(options.find(res => res._id === e.target.value));
+    }
+
+    return (
+        <fieldset className="fieldset">
+            <legend className="fieldset-legend">{text}</legend>
+            <select id={name} name={name} className="select w-100" onChange={changeOption}>
+                {
+                    options.map((option) => (
+                        <option key={option._id || option} value={option._id || option}>{option.name || option}</option>
+                    ))
+                }
+            </select>
+        </fieldset>
+        
+    )
+}
