399dd25ac545dad441633c7bfb48d8ae48d71fb8
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-05 14:00:38 +0200

    implemented table component

--- app/dashboard/bills/page.jsx
+++ app/dashboard/bills/page.jsx
@@ -5,6 +5,7 @@ import AlertError from "@/components/alerts/AlertError";
 import DashTitle from "@/components/DashTitle";
 import { retrieveData } from "@/config/getRequest";
 import AddBillButton from "@/components/buttons/AddBillButton";
+import BillsTable from "@/components/tables/BillsTable";
 
 const apiUrl = process.env.API_URL;
 const billsLimit = 50;
@@ -40,30 +41,7 @@ export default async function BillsPage() {
                     </div>
                 </div>
                 {/* table of 50 recent bills */}
-                <div className="relative overflow-x-auto border-1 border-gray-200 sm:rounded-lg">
-                    <table className="w-full text-sm text-left text-gray-500">
-                        <thead className="text-xs text-gray-700 uppercase bg-base-50">
-                            <tr>
-                                <th scope="col" className="px-6 py-3">Residence</th>
-                                <th scope="col" className="px-6 py-3">Category</th>
-                                <th scope="col" className="px-6 py-3">Amount</th>
-                                <th scope="col" className="px-6 py-3">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                latestBills.map((bill) => (
-                                    <tr key={bill._id} className="odd:bg-white even:bg-gray-50 border-b border-gray-200">
-                                        <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{bill.residence}</td>
-                                        <td className="px-6 py-4">{bill.category}</td>
-                                        <td className="px-6 py-4">{bill.amount}€</td>
-                                        <td className="px-6 py-4">{bill.status}</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                <BillsTable latestBills={latestBills} />
             </div>
         )        
     } catch (error) {
