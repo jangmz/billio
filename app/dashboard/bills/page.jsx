77d17c2e54f2cb0ffbc9768e4240bbbd8d142546
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-05 13:34:28 +0200

    implemented table of latest 50 bills

--- app/dashboard/bills/page.jsx
+++ app/dashboard/bills/page.jsx
@@ -7,6 +7,7 @@ import { retrieveData } from "@/config/getRequest";
 import AddBillButton from "@/components/buttons/AddBillButton";
 
 const apiUrl = process.env.API_URL;
+const billsLimit = 50;
 
 export default async function BillsPage() {
     try {
@@ -18,7 +19,7 @@ export default async function BillsPage() {
         const sessionToken = (await cookieStore).get("authjs.session-token")?.value;
 
         // retrieve user needed data
-        //const { bills } = await retrieveData(sessionToken, "/bills");
+        const { latestBills } = await retrieveData(sessionToken, "/bills/latest", `?limit=${billsLimit}`);
         const { categories } = await retrieveData(sessionToken, "/categories");
         const { residences } = await retrieveData(sessionToken, "/residences", `?u=${session.user.id}`);
 
@@ -37,6 +38,31 @@ export default async function BillsPage() {
                             residences={residences}
                         />
                     </div>
+                </div>
+                {/* table of 50 recent bills */}
+                <div className="relative overflow-x-auto border-1 border-gray-200 sm:rounded-lg">
+                    <table className="w-full text-sm text-left text-gray-500">
+                        <thead className="text-xs text-gray-700 uppercase bg-base-50">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">Residence</th>
+                                <th scope="col" className="px-6 py-3">Category</th>
+                                <th scope="col" className="px-6 py-3">Amount</th>
+                                <th scope="col" className="px-6 py-3">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                latestBills.map((bill) => (
+                                    <tr key={bill._id} className="odd:bg-white even:bg-gray-50 border-b border-gray-200">
+                                        <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{bill.residence}</td>
+                                        <td className="px-6 py-4">{bill.category}</td>
+                                        <td className="px-6 py-4">{bill.amount}€</td>
+                                        <td className="px-6 py-4">{bill.status}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )        
