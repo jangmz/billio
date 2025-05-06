3d2adcf0556c2747d82c5bdcf220bad750b7891c
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-06 13:31:00 +0200

    transformed the main content into client component for better user experience with dynamic refreshing after new data is added

--- app/dashboard/bills/page.jsx
+++ app/dashboard/bills/page.jsx
@@ -1,14 +1,9 @@
 import { validateSession } from "@/config/validateSession";
 import { cookies } from "next/headers";
-import { FaPlus } from "react-icons/fa6";
 import AlertError from "@/components/alerts/AlertError";
-import DashTitle from "@/components/DashTitle";
-import { retrieveData } from "@/config/getRequest";
-import AddBillButton from "@/components/buttons/AddBillButton";
-import BillsTable from "@/components/tables/BillsTable";
+import BillsMainContent from "@/components/mainContent/BillsMainContent";
 
 const apiUrl = process.env.API_URL;
-const billsLimit = 50;
 
 export default async function BillsPage() {
     try {
@@ -18,34 +13,18 @@ export default async function BillsPage() {
         // retrieve cookies
         const cookieStore = cookies();
         const sessionToken = (await cookieStore).get("authjs.session-token")?.value;
+        
+        if (!sessionToken) {
+            throw new Error("Session token is missing");
+        }
 
-        // retrieve user needed data
-        const { latestBills } = await retrieveData(sessionToken, "/bills/latest", `?limit=${billsLimit}`);
-        const { categories } = await retrieveData(sessionToken, "/categories");
-        const { residences } = await retrieveData(sessionToken, "/residences", `?u=${session.user.id}`);
-
-        return (
-            <div className="flex flex-col gap-6">
-                {/* title and top action menu */}
-                <div className="flex justify-between">
-                    <DashTitle title={"Bills"} />
-                    <div className="">
-                        <AddBillButton
-                            text={"New bill"}
-                            icon={<FaPlus />}
-                            apiUrl={apiUrl}
-                            sessionToken={sessionToken}
-                            categories={categories}
-                            residences={residences}
-                        />
-                    </div>
-                </div>
-                {/* table of 50 recent bills */}
-                <BillsTable latestBills={latestBills} />
-            </div>
-        )        
+        return <BillsMainContent 
+            apiUrl={apiUrl}
+            sessionToken={sessionToken} 
+            userId={session.user.id} 
+        />;
     } catch (error) {
         console.error(error);
-        return <AlertError error={error.message} />; 
+        return <AlertError error={error.message} />;
     }
 }
---
+++ components/mainContent/BillsMainContent.jsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { FaPlus } from "react-icons/fa6";
+import AlertError from "@/components/alerts/AlertError";
+import DashTitle from "@/components/DashTitle";
+import { retrieveData } from "@/config/getRequest";
+import AddBillButton from "@/components/buttons/AddBillButton";
+import BillsTable from "@/components/tables/BillsTable";
+import { useState, useEffect } from "react";
+
+const billsLimit = 50;
+
+export default function BillsMainContent({ apiUrl, sessionToken, userId }) {
+    const [bills, setBills] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [residences, setResidences] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {          
+                console.log("Fetching data...");      
+                // retrieve user needed data
+                const { latestBills } = await retrieveData(sessionToken, `${apiUrl}/bills/latest`, `?limit=${billsLimit}`);
+                const { categories } = await retrieveData(sessionToken, `${apiUrl}/categories`);
+                const { residences } = await retrieveData(sessionToken, `${apiUrl}/residences`, `?u=${userId}`);
+
+                console.log("Bills:", latestBills);
+                console.log("Categories:", categories);
+                console.log("Residences:", residences);                
+
+                setBills(latestBills);
+                setCategories(categories);
+                setResidences(residences);
+            } catch (error) {
+                console.error(error);
+                setError(error.message);
+            }
+        }
+
+        fetchData();
+    }, [sessionToken, userId]);
+
+    // update bills on insert
+    function handleAddBill(newBill) {
+        setBills((prevBills) => [newBill, ...prevBills]);
+    }
+
+    if (error) {
+        return <AlertError error={error} />;
+    }
+
+    return (
+        <div className="flex flex-col gap-6">
+            {/* title and top action menu */}
+            <div className="flex justify-between">
+                <DashTitle title={"Bills"} />
+                <div className="">
+                    <AddBillButton
+                        text={"New bill"}
+                        icon={<FaPlus />}
+                        apiUrl={apiUrl}
+                        sessionToken={sessionToken}
+                        categories={categories}
+                        residences={residences}
+                        onAddBill={handleAddBill} // passing callback for updating state on new insert
+                    />
+                </div>
+            </div>
+            {/* table of 50 recent bills */}
+            <BillsTable bills={bills} />
+        </div>
+    );
+}
