0e396c01eb0221541dfbdccd8c64d67d393254f9
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-14 11:00:50 +0200

    session validation, added components

--- app/dashboard/support-feedback/page.jsx
+++ app/dashboard/support-feedback/page.jsx
@@ -1,10 +1,14 @@
+import AlertError from "@/components/alerts/AlertError";
 import ModalCard from "@/components/cards/ModalCard";
 import ContactForm from "@/components/forms/ContactForm";
 import FeedbackForm from "@/components/forms/FeedbackForm";
 import SuggestionsList from "@/components/SuggestionsList";
+import { validateSession } from "@/config/validateSession";
+import { cookies } from "next/headers";
 import { GrContact } from "react-icons/gr";
 import { VscFeedback } from "react-icons/vsc";
 
+const apiUrl = process.env.API_URL;
 const test = [
   {
     name: "test name",
@@ -13,24 +17,40 @@ const test = [
   }
 ]
 
-export default function SupportFeedbackPage() {
-  return (
-    <div className="flex flex-col items-center gap-10 p-6">
-      <div className="grid gap-10 grid-cols-1 md:grid-cols-2">
-        <ModalCard 
-          icon={<GrContact />}
-          title={"Contact Us"}
-          modalName={"contact"}
-          content={<ContactForm />}
-        />
-        <ModalCard 
-          icon={<VscFeedback />}
-          title={"Submit feedback"}
-          modalName={"feedback"}
-          content={<FeedbackForm />}
-        />
+export default async function SupportFeedbackPage() {
+  try {
+    // validate session
+    const session = await validateSession();
+
+    // retrieve cookies
+    const cookieStore = cookies();
+    const sessionToken = (await cookieStore).get("authjs.session-token")?.value;
+
+    if (!sessionToken) {
+      throw new Error("Session token is missing");
+    }
+
+    return (
+      <div className="flex flex-col items-center gap-10 p-6">
+        <div className="grid gap-10 grid-cols-1 md:grid-cols-2">
+          <ModalCard 
+            icon={<GrContact />}
+            title={"Contact Us"}
+            modalName={"contact"}
+            content={<ContactForm user={session.user} apiUrl={apiUrl} sessionToken={sessionToken} />}
+          />
+          <ModalCard 
+            icon={<VscFeedback />}
+            title={"Submit feedback"}
+            modalName={"feedback"}
+            content={<FeedbackForm user={session.user} apiUrl={apiUrl} sessionToken={sessionToken} />}
+          />
+        </div>
+        <SuggestionsList suggestionsArr={test} />
       </div>
-      <SuggestionsList suggestionsArr={test} />
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error(error);
+    return <AlertError error={error.message} />;
+  }
 }
