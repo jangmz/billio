1e1997b0456b7387d815d904a50058d5b6e028ed
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-15 14:06:35 +0200

    coming soon page

--- app/dashboard/reports/page.jsx
+++ app/dashboard/reports/page.jsx
@@ -1,5 +1,11 @@
+import AlertInfo from "@/components/alerts/AlertInfo";
+import DashTitle from "@/components/DashTitle";
+
 export default function ReportsPage() {
   return (
-    <div>ReportsPage</div>
-  )
+    <>
+      <DashTitle title={"Reports"} />
+      <AlertInfo information={"This page is coming soon."} />
+    </>
+  );
 }
