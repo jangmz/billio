b963824cce70dc25eebb0f225788f1a5e67cadd2
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-16 13:52:09 +0200

    added icons

--- components/forms/LogInProviders.jsx
+++ components/forms/LogInProviders.jsx
@@ -1,17 +1,23 @@
 import Button from "../buttons/Button";
 
 import { loginGitHub } from "@/_actions/authActions";
+import { FaGithub } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
 
 export default function LogInProviders() {
     return (
         <div className="flex flex-col gap-3">
             <Button 
-                text="Log in with GitHub"
+                icon={<FaGoogle />}
+                text={"Log in with Google"}
                 btnStyle={"btn-primary"}
-                onClick={loginGitHub}
+                
             />
             <Button 
-                text={"Log in with Google"}
+                text="Log in with GitHub"
+                icon={<FaGithub />}
+                btnStyle={"btn-primary"}
+                onClick={loginGitHub}
             />
         </div>
     );
