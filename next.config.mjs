3f6e66332319ba690d575ad191c2027cb0de6d93
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-23 15:19:03 +0200

    docker file

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
     images: {
         domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
-    }
+    },
+    output: "standalone",
 };
 
 export default nextConfig;
