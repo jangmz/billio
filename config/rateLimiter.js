8d8df6c857961797ca5c4df9a7313728f83f9a3f
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-29 14:11:18 +0200

    limiting to 120 requests per 60 seconds

--- config/rateLimiter.js
+++ config/rateLimiter.js
@@ -1,6 +1,6 @@
 import { RateLimiterMemory } from "rate-limiter-flexible";
 
 export const rateLimiter = new RateLimiterMemory({
-    points: 10, // 10 points
+    points: 120, // 120 requests per minute
     duration: 60 // per 60 sec
 });
