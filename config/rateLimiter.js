fd30b63917133c9121b4768f0127eca99183f488
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-05-30 12:53:22 +0200

    60 req per minute

--- config/rateLimiter.js
+++ config/rateLimiter.js
@@ -1,6 +1,6 @@
 import { RateLimiterMemory } from "rate-limiter-flexible";
 
 export const rateLimiter = new RateLimiterMemory({
-    points: 60, // 120 requests per minute
+    points: 60, // 60 requests per minute
     duration: 60 // per 60 sec
 });
